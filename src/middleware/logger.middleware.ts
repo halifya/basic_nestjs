c1eca548b453945dba9584878e0f38abf492cf53
Halif Yama <111275252+halifya@users.noreply.github.com>
2024-01-20 22:50:57 +0700

    fix(log console): fix bug
    fix bug log wrong respose value

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -4,11 +4,23 @@ import { LoggerService } from '../utils/logger/logger.service';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  private logger = new LoggerService
-   
-  use(req: Request, res: Response, next: NextFunction) {
+  private logger = new LoggerService();
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, baseUrl } = request;
+
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('close', () => {
+      const { statusCode } = response;
+
+      this.logger.log(
+        `${method} ${baseUrl}   ${statusCode} - ${userAgent} ${ip}`, "HTTP"
+      );
+    });
+
     next();
-    this.logger.log(`HTTP  request[${res.statusCode}]  ${req.method}   ${req.url}`,"LOG-MIDDLEWARE");
-    
   }
+
+
 }
